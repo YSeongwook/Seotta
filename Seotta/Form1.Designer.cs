942c07d81fd2b9fbe53bd66a68c5c74073e30707
Seongwook <9892yyyssww@gmail.com>
2024-03-14 23:23:25 +0900

    Seotta

---
+++ Seotta/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Seotta
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 4);
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(10, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(354, 554); // 텍스트 박스의 크기와 위치를 조정하세요.
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 4);
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(380, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(354, 554); // 텍스트 박스의 크기와 위치를 조정하세요.
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.Font = new System.Drawing.Font("Consolas", 4);
+            this.textBox3.ForeColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(750, 12);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(354, 554); // 텍스트 박스의 크기와 위치를 조정하세요.
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Black;
+            this.textBox4.Font = new System.Drawing.Font("Consolas", 4);
+            this.textBox4.ForeColor = System.Drawing.Color.White;
+            this.textBox4.Location = new System.Drawing.Point(1120, 12);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(354, 554); // 텍스트 박스의 크기와 위치를 조정하세요.
+            this.textBox4.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1920, 1080); // 폼의 크기를 조정하세요.
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
---
+++ Seotta/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Seotta
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer1;
+        private Timer timer2;
+
+        private Timer[] timer;
+        private string[] lines1;
+        private string[] lines2;
+        private string[] lines3;
+        private string[] lines4;
+        private int currentIndex1 = 0;
+        private int currentIndex2 = 0;
+        private int currentIndex3 = 0;
+        private int currentIndex4 = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 폼의 배경색을 검정색으로 설정
+            this.BackColor = Color.Black;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 타이머1 설정
+            timer1 = new Timer();
+            timer1.Interval = 1; // 1 밀리초마다 변경
+            timer1.Tick += Timer1_Tick;
+
+            // 타이머2 설정
+            timer2 = new Timer();
+            timer2.Interval = 1; // 1 밀리초마다 변경
+            timer2.Tick += Timer2_Tick;
+
+            // 텍스트 파일에서 줄 읽어오기
+            lines1 = ReadAllLinesFromFile("1.txt");
+            lines2 = ReadAllLinesFromFile("2.txt");
+            lines3 = ReadAllLinesFromFile("3.txt");
+            lines4 = ReadAllLinesFromFile("4.txt");
+
+            // 타이머 시작
+            timer1.Start();
+        }
+
+        private string[] ReadAllLinesFromFile(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                return File.ReadAllLines(fileName);
+            }
+            else
+            {
+                Console.WriteLine($"File not found: {fileName}");
+                return new string[0];
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (currentIndex1 < lines1.Length)
+            {
+                textBox1.AppendText(lines1[currentIndex1] + Environment.NewLine);
+                textBox3.AppendText(lines3[currentIndex3] + Environment.NewLine);
+                currentIndex1++;
+                currentIndex3++;
+            }
+            else
+            {
+                // 타이머1 중지 및 타이머2 시작
+                timer1.Stop();
+                timer2.Start();
+            }
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            if (currentIndex2 < lines2.Length)
+            {
+                textBox2.AppendText(lines2[currentIndex2] + Environment.NewLine);
+                textBox4.AppendText(lines4[currentIndex4] + Environment.NewLine);
+                currentIndex2++;
+                currentIndex4++;
+            }
+            else
+            {
+                // 타이머2 중지
+                timer2.Stop();
+            }
+        }
+    }
+}
