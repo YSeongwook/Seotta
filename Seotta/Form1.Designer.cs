22fbe320d12d4d907d9ca452db8a23f51b6e5882
SeongWook <9892yyyssww@gmail.com>
2024-03-18 11:13:05 +0900

    03.18 11:12
    Creating Right Button..

--- Seotta/Form1.Designer.cs
+++ Seotta/Form1.Designer.cs
@@ -20,183 +20,249 @@ namespace Seotta
 
         private void InitializeComponent()
         {
-            this.labelTitle = new System.Windows.Forms.Label(); // 게임 제목
-            this.pae1 = new System.Windows.Forms.TextBox();     // 화투패
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.pae1 = new System.Windows.Forms.TextBox();
             this.pae2 = new System.Windows.Forms.TextBox();
             this.pae3 = new System.Windows.Forms.TextBox();
             this.pae4 = new System.Windows.Forms.TextBox();
-            this.gameProgress = new System.Windows.Forms.TextBox();  // 게임 진행 안내문
+            this.gameProgress = new System.Windows.Forms.TextBox();
+            this.betBtn7 = new System.Windows.Forms.Button();
+            this.betBtn8 = new System.Windows.Forms.Button();
+            this.betBtn4 = new System.Windows.Forms.Button();
+            this.betBtn5 = new System.Windows.Forms.Button();
+            this.betBtn1 = new System.Windows.Forms.Button();
+            this.betBtn0 = new System.Windows.Forms.Button();
+            this.jokboHelpBtn = new System.Windows.Forms.Button();
+            this.betHelpBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
-
+            // 
             // labelTitle
+            // 
             this.labelTitle.AutoSize = true;
-            this.labelTitle.Font = new Font("Consolas", 32, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0)));
-            this.labelTitle.ForeColor = Color.White;
-            this.labelTitle.Location = new Point(688, 20);
+            this.labelTitle.Font = new System.Drawing.Font("Consolas", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.ForeColor = System.Drawing.Color.White;
+            this.labelTitle.Location = new System.Drawing.Point(860, 24);
+            this.labelTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelTitle.Name = "labelTitle";
-            this.labelTitle.Size = new Size(124, 240);
+            this.labelTitle.Size = new System.Drawing.Size(140, 75);
             this.labelTitle.TabIndex = 5;
             this.labelTitle.Text = "섰다";
-            this.labelTitle.TextAlign = ContentAlignment.MiddleCenter;
-
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // pae1
-            this.pae1.BackColor = Color.Black;
-            this.pae1.Font = new Font("Consolas", 4);
-            this.pae1.ForeColor = Color.White;
-            this.pae1.Location = new Point(10, 100);
+            // 
+            this.pae1.BackColor = System.Drawing.Color.Black;
+            this.pae1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.pae1.Font = new System.Drawing.Font("Consolas", 4F);
+            this.pae1.ForeColor = System.Drawing.Color.White;
+            this.pae1.Location = new System.Drawing.Point(12, 120);
+            this.pae1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pae1.Multiline = true;
             this.pae1.Name = "pae1";
-            this.pae2.TabIndex = 1;
-            this.pae1.Size = new Size(354, 554);
-            this.pae1.BorderStyle = BorderStyle.None;
-            this.pae1.ReadOnly = true;  // TextBox를 읽기 전용으로 설정
+            this.pae1.ReadOnly = true;
+            this.pae1.Size = new System.Drawing.Size(442, 665);
+            this.pae1.TabIndex = 5;
             this.pae1.TabStop = false;
-
+            // 
             // pae2
-            this.pae2.BackColor = Color.Black;
-            this.pae2.Font = new Font("Consolas", 4);
-            this.pae2.ForeColor = Color.White;
-            this.pae2.Location = new Point(380, 100);
+            // 
+            this.pae2.BackColor = System.Drawing.Color.Black;
+            this.pae2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.pae2.Font = new System.Drawing.Font("Consolas", 4F);
+            this.pae2.ForeColor = System.Drawing.Color.White;
+            this.pae2.Location = new System.Drawing.Point(475, 120);
+            this.pae2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pae2.Multiline = true;
             this.pae2.Name = "pae2";
-            this.pae2.Size = new Size(354, 554);
-            this.pae2.TabIndex = 2;
-            this.pae2.BorderStyle = BorderStyle.None;
             this.pae2.ReadOnly = true;
+            this.pae2.Size = new System.Drawing.Size(442, 665);
+            this.pae2.TabIndex = 2;
             this.pae2.TabStop = false;
-
+            // 
             // pae3
-            this.pae3.BackColor = Color.Black;
-            this.pae3.Font = new Font("Consolas", 4);
-            this.pae3.ForeColor = Color.White;
-            this.pae3.Location = new Point(750, 100);
+            // 
+            this.pae3.BackColor = System.Drawing.Color.Black;
+            this.pae3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.pae3.Font = new System.Drawing.Font("Consolas", 4F);
+            this.pae3.ForeColor = System.Drawing.Color.White;
+            this.pae3.Location = new System.Drawing.Point(938, 120);
+            this.pae3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pae3.Multiline = true;
             this.pae3.Name = "pae3";
-            this.pae3.Size = new Size(354, 554);
-            this.pae3.TabIndex = 3;
-            this.pae3.BorderStyle = BorderStyle.None;
             this.pae3.ReadOnly = true;
+            this.pae3.Size = new System.Drawing.Size(442, 665);
+            this.pae3.TabIndex = 3;
             this.pae3.TabStop = false;
-
+            // 
             // pae4
-            this.pae4.BackColor = Color.Black;
-            this.pae4.Font = new Font("Consolas", 4);
-            this.pae4.ForeColor = Color.White;
-            this.pae4.Location = new Point(1120, 100);
+            // 
+            this.pae4.BackColor = System.Drawing.Color.Black;
+            this.pae4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.pae4.Font = new System.Drawing.Font("Consolas", 4F);
+            this.pae4.ForeColor = System.Drawing.Color.White;
+            this.pae4.Location = new System.Drawing.Point(1400, 120);
+            this.pae4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pae4.Multiline = true;
             this.pae4.Name = "pae4";
-            this.pae4.Size = new Size(354, 554);
-            this.pae4.TabIndex = 4;
-            this.pae4.BorderStyle = BorderStyle.None;
             this.pae4.ReadOnly = true;
+            this.pae4.Size = new System.Drawing.Size(442, 665);
+            this.pae4.TabIndex = 4;
             this.pae4.TabStop = false;
-
+            // 
             // gameProgress
-            this.gameProgress.BackColor = Color.Black;
-            this.gameProgress.Font = new Font("Consolas", 24F);
-            this.gameProgress.ForeColor = Color.White;
-            this.gameProgress.Location = new Point(10, 664);
+            // 
+            this.gameProgress.BackColor = System.Drawing.Color.Black;
+            this.gameProgress.Font = new System.Drawing.Font("Consolas", 24F);
+            this.gameProgress.ForeColor = System.Drawing.Color.White;
+            this.gameProgress.Location = new System.Drawing.Point(12, 797);
+            this.gameProgress.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gameProgress.Multiline = true;
             this.gameProgress.Name = "gameProgress";
-            this.gameProgress.Size = new Size(1454, 375);
-            this.gameProgress.TabIndex = 0; // TabIndex를 0으로 설정해 폼이 생성되면 포커스가 자동으로 할당
-            this.gameProgress.TabStop = false;
             this.gameProgress.ReadOnly = true;
-            this.gameProgress.ScrollBars = ScrollBars.Vertical;
-
-            // bettingButton
-            this.betBtn7 = new System.Windows.Forms.Button();
-            this.betBtn7.Location = new System.Drawing.Point(1480, 660); // 버튼 위치 설정
+            this.gameProgress.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.gameProgress.Size = new System.Drawing.Size(1816, 449);
+            this.gameProgress.TabIndex = 0;
+            this.gameProgress.TabStop = false;
+            // 
+            // betBtn7
+            // 
+            this.betBtn7.Font = new System.Drawing.Font("Consolas", 20F);
+            this.betBtn7.ForeColor = System.Drawing.Color.White;
+            this.betBtn7.Location = new System.Drawing.Point(1850, 792);
+            this.betBtn7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.betBtn7.Name = "betBtn7";
-            this.betBtn7.Size = new System.Drawing.Size(200, 80); // 버튼 크기 설정
-            this.betBtn7.Text = "7|   콜  "; // 버튼 텍스트 설정
-            this.betBtn7.TextAlign = ContentAlignment.MiddleCenter;
-            this.betBtn7.Font = new Font("Consolas", 20);
-            this.betBtn7.ForeColor = Color.White;
+            this.betBtn7.Size = new System.Drawing.Size(250, 96);
             this.betBtn7.TabIndex = 5;
-            this.Controls.Add(this.betBtn7); // 폼에 버튼 추가
-            
-            this.betBtn8 = new System.Windows.Forms.Button();
-            this.betBtn8.Location = new System.Drawing.Point(1700, 660); // 버튼 위치 설정
+            this.betBtn7.TabStop = false;
+            this.betBtn7.Text = "7|   콜  ";
+            // 
+            // betBtn8
+            // 
+            this.betBtn8.Font = new System.Drawing.Font("Consolas", 20F);
+            this.betBtn8.ForeColor = System.Drawing.Color.White;
+            this.betBtn8.Location = new System.Drawing.Point(2112, 792);
+            this.betBtn8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.betBtn8.Name = "betBtn8";
-            this.betBtn8.Size = new System.Drawing.Size(200, 80); // 버튼 크기 설정
-            this.betBtn8.Text = "8|  다이 "; // 버튼 텍스트 설정
-            this.betBtn8.TextAlign = ContentAlignment.MiddleCenter;
-            this.betBtn8.Font = new Font("Consolas", 20);
-            this.betBtn8.ForeColor = Color.White;
-            this.betBtn8.Padding = new Padding(0, 0, 10, 0);
+            this.betBtn8.Padding = new System.Windows.Forms.Padding(0, 0, 12, 0);
+            this.betBtn8.Size = new System.Drawing.Size(250, 96);
             this.betBtn8.TabIndex = 6;
-            this.Controls.Add(this.betBtn8); // 폼에 버튼 추가
-
-            this.betBtn4 = new System.Windows.Forms.Button();
-            this.betBtn4.Location = new System.Drawing.Point(1480, 760); // 버튼 위치 설정
+            this.betBtn8.TabStop = false;
+            this.betBtn8.Text = "8|  다이 ";
+            // 
+            // betBtn4
+            // 
+            this.betBtn4.Font = new System.Drawing.Font("Consolas", 20F);
+            this.betBtn4.ForeColor = System.Drawing.Color.White;
+            this.betBtn4.Location = new System.Drawing.Point(1850, 912);
+            this.betBtn4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.betBtn4.Name = "betBtn4";
-            this.betBtn4.Size = new System.Drawing.Size(200, 80); // 버튼 크기 설정
-            this.betBtn4.Text = "4|   삥  "; // 버튼 텍스트 설정
-            this.betBtn4.TextAlign = ContentAlignment.MiddleCenter;
-            this.betBtn4.Font = new Font("Consolas", 20);
-            this.betBtn4.ForeColor = Color.White;
+            this.betBtn4.Size = new System.Drawing.Size(250, 96);
             this.betBtn4.TabIndex = 7;
-            this.Controls.Add(this.betBtn4); // 폼에 버튼 추가
-
-            this.betBtn5 = new System.Windows.Forms.Button();
-            this.betBtn5.Location = new System.Drawing.Point(1700, 760); // 버튼 위치 설정
+            this.betBtn4.TabStop = false;
+            this.betBtn4.Text = "4|   삥  ";
+            // 
+            // betBtn5
+            // 
+            this.betBtn5.Font = new System.Drawing.Font("Consolas", 20F);
+            this.betBtn5.ForeColor = System.Drawing.Color.White;
+            this.betBtn5.Location = new System.Drawing.Point(2112, 912);
+            this.betBtn5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.betBtn5.Name = "betBtn5";
-            this.betBtn5.Size = new System.Drawing.Size(200, 80); // 버튼 크기 설정
-            this.betBtn5.Text = "5|  체크 "; // 버튼 텍스트 설정
-            this.betBtn5.TextAlign = ContentAlignment.MiddleCenter;
-            this.betBtn5.Font = new Font("Consolas", 20);
-            this.betBtn5.ForeColor = Color.White;
-            this.betBtn5.Padding = new Padding(0, 0, 10, 0);
+            this.betBtn5.Padding = new System.Windows.Forms.Padding(0, 0, 12, 0);
+            this.betBtn5.Size = new System.Drawing.Size(250, 96);
             this.betBtn5.TabIndex = 8;
-            this.Controls.Add(this.betBtn5); // 폼에 버튼 추가
-
-            this.betBtn1 = new System.Windows.Forms.Button();
-            this.betBtn1.Location = new System.Drawing.Point(1480, 860); // 버튼 위치 설정
+            this.betBtn5.TabStop = false;
+            this.betBtn5.Text = "5|  체크 ";
+            // 
+            // betBtn1
+            // 
+            this.betBtn1.Font = new System.Drawing.Font("Consolas", 20F);
+            this.betBtn1.ForeColor = System.Drawing.Color.White;
+            this.betBtn1.Location = new System.Drawing.Point(1850, 1032);
+            this.betBtn1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.betBtn1.Name = "betBtn1";
-            this.betBtn1.Size = new System.Drawing.Size(200, 80); // 버튼 크기 설정
-            this.betBtn1.Text = "1|  따당 "; // 버튼 텍스트 설정
-            this.betBtn1.TextAlign = ContentAlignment.MiddleCenter;
-            this.betBtn1.Font = new Font("Consolas", 20);
-            this.betBtn1.ForeColor = Color.White;
-            this.betBtn1.Padding = new Padding(0, 0, 10, 0);
+            this.betBtn1.Padding = new System.Windows.Forms.Padding(0, 0, 12, 0);
+            this.betBtn1.Size = new System.Drawing.Size(250, 96);
             this.betBtn1.TabIndex = 9;
-            this.Controls.Add(this.betBtn1); // 폼에 버튼 추가
-
-            this.betBtn0 = new System.Windows.Forms.Button();
-            this.betBtn0.Location = new System.Drawing.Point(1480, 960); // 버튼 위치 설정
+            this.betBtn1.TabStop = false;
+            this.betBtn1.Text = "1|  따당 ";
+            // 
+            // betBtn0
+            // 
+            this.betBtn0.Font = new System.Drawing.Font("Consolas", 20F);
+            this.betBtn0.ForeColor = System.Drawing.Color.White;
+            this.betBtn0.Location = new System.Drawing.Point(1850, 1152);
+            this.betBtn0.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.betBtn0.Name = "betBtn0";
-            this.betBtn0.Size = new System.Drawing.Size(200, 80); // 버튼 크기 설정
-            this.betBtn0.Text = "0|  하프 "; // 버튼 텍스트 설정
-            this.betBtn0.TextAlign = ContentAlignment.MiddleCenter;
-            this.betBtn0.Font = new Font("Consolas", 20);
-            this.betBtn0.ForeColor = Color.White;
-            this.betBtn0.Padding = new Padding(0, 0, 10, 0);
+            this.betBtn0.Padding = new System.Windows.Forms.Padding(0, 0, 12, 0);
+            this.betBtn0.Size = new System.Drawing.Size(250, 96);
             this.betBtn0.TabIndex = 10;
-            this.Controls.Add(this.betBtn0); // 폼에 버튼 추가
+            this.betBtn0.TabStop = false;
+            this.betBtn0.Text = "0|  하프 ";
+            // 
+            // jokboHelpBtn
+            // 
+            this.jokboHelpBtn.Font = new System.Drawing.Font("Consolas", 15F);
+            this.jokboHelpBtn.ForeColor = System.Drawing.Color.White;
+            this.jokboHelpBtn.Location = new System.Drawing.Point(2016, 24);
+            this.jokboHelpBtn.Name = "jokboHelpBtn";
+            this.jokboHelpBtn.Size = new System.Drawing.Size(168, 58);
+            this.jokboHelpBtn.TabIndex = 11;
+            this.jokboHelpBtn.TabStop = false;
+            this.jokboHelpBtn.Text = "족보 도움말";
+            this.jokboHelpBtn.Click += new System.EventHandler(this.JokboHelpBtn_BtnClick);
 
+            // betHelpBtn
+            this.betHelpBtn.Font = new System.Drawing.Font("Consolas", 15F);
+            this.betHelpBtn.ForeColor = System.Drawing.Color.White;
+            this.betHelpBtn.Location = new System.Drawing.Point(2191, 24);
+            this.betHelpBtn.Name = "betHelpBtn";
+            this.betHelpBtn.Size = new System.Drawing.Size(168, 58);
+            this.betHelpBtn.TabIndex = 12;
+            this.betHelpBtn.TabStop = false;
+            this.betHelpBtn.TextAlign = ContentAlignment.MiddleCenter;
+            this.betHelpBtn.Text = "베팅 도움말";
+            this.betHelpBtn.Click += new System.EventHandler(this.BetHelpBtn_BtnClick);
+            // 
             // Form1
-            this.AutoScaleDimensions = new SizeF(8F, 15F);
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new Size(1920, 1080);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(2400, 1296);
             this.Controls.Add(this.gameProgress);
             this.Controls.Add(this.pae4);
             this.Controls.Add(this.pae3);
             this.Controls.Add(this.pae2);
             this.Controls.Add(this.pae1);
             this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.betBtn7);
+            this.Controls.Add(this.betBtn8);
+            this.Controls.Add(this.betBtn4);
+            this.Controls.Add(this.betBtn5);
+            this.Controls.Add(this.betBtn1);
+            this.Controls.Add(this.betBtn0);
+            this.Controls.Add(this.jokboHelpBtn);
+            this.Controls.Add(this.betHelpBtn);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
+        // 게임 제목 레이블
         private System.Windows.Forms.Label labelTitle;
+
+        // 패 텍스트 박스
         private System.Windows.Forms.TextBox pae1;
         private System.Windows.Forms.TextBox pae2;
         private System.Windows.Forms.TextBox pae3;
         private System.Windows.Forms.TextBox pae4;
+
+        // 게임 안내 텍스트 박스
         private System.Windows.Forms.TextBox gameProgress;
 
         // 베팅 버튼
@@ -206,6 +272,9 @@ namespace Seotta
         private System.Windows.Forms.Button betBtn5;
         private System.Windows.Forms.Button betBtn1;
         private System.Windows.Forms.Button betBtn0;
-        private System.Windows.Forms.Button betBtn7Call;
+
+        // 도움말 버튼
+        private System.Windows.Forms.Button jokboHelpBtn;
+        private System.Windows.Forms.Button betHelpBtn;
     }
 }
--- Seotta/Form1.cs
+++ Seotta/Form1.cs
@@ -9,7 +9,10 @@ namespace Seotta
 {
     public partial class Form1 : Form
     {
-      Game game;
+        Game game;
+
+        string[] jokbo = {"1.38광땡", "2.광땡", "3.땡", "4.알리", "5.독사", "6.구삥", "7.장삥",
+            "8.장사", "9.세륙", "10.갑오", "11.끗,망통"," * 구사", "* 땡잡이", "* 암행어사"};
 
         public Form1()
         {
@@ -23,6 +26,51 @@ namespace Seotta
 
             // KeyPreview를 true로 설정하여 폼이 키 이벤트를 먼저 받도록 함
             this.KeyPreview = true;
+
+            CreateJokboButton();
+        }
+
+        private void CreateJokboButton()
+        {
+            // 패널 생성
+            Panel panel = new Panel();
+            panel.Location = new Point(1278, 50); // 좌상단 위치 설정
+            panel.Size = new Size(380, 250); // 크기 설정
+            this.Controls.Add(panel); // 폼에 패널 추가
+
+            // 버튼 생성 및 패널에 추가
+            int buttonWidth = 118;
+            int buttonHeight = 40;
+            int gap = 5; // 버튼 간격
+            int row = 5;
+            int col = 3;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Button button = new Button();
+                    button.Text = $"Button {i * col + j + 1}";
+                    button.ForeColor = Color.White;
+                    button.TextAlign = ContentAlignment.MiddleLeft;
+                    button.Padding = new Padding(8, 0, 0, 0);
+                    button.Location = new Point(10 + j * (buttonWidth + gap), 30 + i * (buttonHeight + gap)); // 좌상단 위치 설정
+                    button.Size = new Size(buttonWidth, buttonHeight); // 크기 설정
+                    panel.Controls.Add(button); // 패널에 버튼 추가
+                }
+            }
+
+            Button jokboLabel = (Button)panel.Controls[0];
+            jokboLabel.TextAlign = ContentAlignment.MiddleCenter;
+            jokboLabel.FlatStyle = FlatStyle.Flat;
+            jokboLabel.FlatAppearance.BorderSize = 0; // 테두리 크기를 0으로 설정하여 테두리를 없앰
+            jokboLabel.Text = "족보";
+            jokboLabel.Font = new Font("Consolas", 22F);
+
+            for (int i = 1; i < panel.Controls.Count; i++)
+            {
+                panel.Controls[i].Font = new Font("Consolas", 14F);
+                panel.Controls[i].Text = jokbo[i - 1];
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -30,9 +78,9 @@ namespace Seotta
             game = new Game(this, pae1, pae2, pae3, pae4, gameProgress);
             game.StartGame();
 
-            gameProgress.AppendText(this.Focus().ToString());   // 현재 false임
+            this.Focus();
             this.KeyDown += new KeyEventHandler(Form1_KeyDown);
-            gameProgress.KeyDown += new KeyEventHandler(gameProgress_KeyDown);  // 이벤트 핸들러 만들어서 등록하니 엔터키 입력 됌
+            gameProgress.KeyDown += new KeyEventHandler(GameProgress_KeyDown);  // 이벤트 핸들러 만들어서 등록하니 엔터키 이벤트 성공
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -46,8 +94,7 @@ namespace Seotta
             }
         }
 
-        // 
-        private void gameProgress_KeyDown(object sender, KeyEventArgs e)
+        private void GameProgress_KeyDown(object sender, KeyEventArgs e)
         {
             // Enter 키를 눌렀을 때
             if (e.KeyCode == Keys.Enter)
@@ -56,6 +103,44 @@ namespace Seotta
                 game.ResetPae();     // 패 초기화
                 game.SelectPae();    // 패 선택
             }
+        }
+
+        private void JokboHelpBtn_BtnClick(object sender, EventArgs e)
+        {
+            gameProgress.Text = "족보 도움말\r\n\r\n" +
+                "섯다에서 사용되는 화투는 1월부터 10월까지의 패 중 피를 제외한 두 장씩 모두 20장입니다.\r\n" +
+                "이 20장에서 두 장을 사용하여 족보를 만들어 베팅을 하게 됩니다.\r\n" +
+                "게임 중 족보는 게임화면 우측 상단에서 확인 할 수 있습니다.\r\n\r\n" +
+                "삼팔광땡\r\n섯다에서 가능한 3광과 8광으로 된 최강의 족보입니다.\r\n\r\n" +
+                "광땡\r\n셋 중 2개만 모으면 최고의 족보로,\r\n1광과 3광 또는 1광과 8광으로 된 삼팔 광땡 다음으로 좋은 족보입니다\r\n\r\n" +
+                "땡\r\n같은 월의 조합입니다.\r\n땡 끼리 만났을 경우에는 장땡(10월) ~ 삥땡(1월) 순으로 숫자가 높을수록 이깁니다.\r\n\r\n" +
+                "알리\r\n순서와 패 상관없이 1월과 2월의 조합입니다.\r\n\r\n" +
+                "독사\r\n순서와 패 상관없이 1월과 4월의 조합입니다.\r\n\r\n" +
+                "구삥\r\n순서와 패 상관없이 1월과 9월의 조합입니다.\r\n\r\n" +
+                "장삥\r\n순서와 패 상관없이 1월과 10월의 조합입니다.\r\n\r\n" +
+                "장사\r\n순서와 패 상관없이 4월과 10월의 조합입니다.\r\n\r\n" +
+                "세륙\r\n순서와 패 상관없이 4월과 6월의 조합입니다.\r\n\r\n" +
+                "갑오\r\n두 장의 숫자를 합한 수의 끝자리 숫자(끗수)가 9인 경우입니다.\r\n(예: 1+8, 2+7, 3+6, 4+5 )\r\n\r\n" +
+                "끗\r\n끗수가 8부터 1일 때 각각 8끗, … , 1 끗이라고 하며 숫자가 높을수록 좋은 족보입니다.\r\n\r\n" +
+                "망통\r\n2월과 8월의 조합으로 끗수가 0인 가장 낮은 족보 입니다\r\n\r\n" +
+                "특수 기능 족보\r\n일반 족보 상황 중, 특정 족보에만 적용되는 특수 기능입니다.\r\n\r\n" +
+                "땡잡이\r\n3월과 7월의 조합으로서 구땡 이하의 족보를 이길(잡을) 수 있습니다.\r\n장땡과 광땡,무적의 삼팔 광땡은 잡을 수 없습니다.\r\n만약 상대방 중에 땡이 없다면 끗수가 0이므로망통으로 계산됩니다.\r\n\r\n" +
+                "구사\r\n4월과 9월의 조합으로서 알리 이하의 족보와\r\n(즉, 상대방의 족보 중 가장 높은 것이 알리 이하일 때)\r\n이번 판을 물리고 재경기를 할 수 있습니다.\r\n\r\n" +
+                "멍텅구리 구사\r\n열자리 4월과 열자리 9월로 된 조합으로서 9땡 이하의 족보와\r\n(즉, 상대방의 족보 중 가장 높은 것이 9땡 이하 일 때) 재경기를 할 수 있습니다\r\n\r\n" +
+                "암행어사\r\n열자리 4월과 열자리 7로 된 조합으로서 일삼광땡 혹은 일팔광땡을 이길 수 있습니다.\r\n만약 상대방 중에 광땡이 없다면 1끗으로 계산 됩니다";
+        }
+
+        private void BetHelpBtn_BtnClick(object sender, EventArgs e)
+        {
+            gameProgress.Text = "베팅 도움말\r\n\r\n" +
+                "오른쪽 버튼에 있는 숫자는 숫자 키패드에 대응합니다.\r\n" +
+                "즉, 마우스 클릭 대신에 숫자키를 이용하여 해당 베팅을 할 수 있습니다.\r\n\r\n" +
+                "콜(7): 앞 사람이 베팅한 금액만큼 레이스 머니를 받습니다.\r\n" +
+                "삥(4): 기본 머니 만큼 베팅합니다.\r\n" +
+                "다이(8): 앞사람이 베팅한 금액을 받지 않고, 이번 판을 포기합니다.\r\n" +
+                "체크(5): 머니를 베팅하지 않고 다음 사람의 턴으로 넘깁니다.\r\n" +
+                "따당(1): 앞사람이 베팅한 금액의 2배를 베팅합니다.\r\n" +
+                "하프(0): 앞사람이 베팅한 금액을 받고, 이를 포함한 전체 판돈의 1/2를 추가로 베팅합니다.\r\n";
         }
     }
 }
